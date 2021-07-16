b935a3f94fcfbd7e5d09c8636b88e5bf0b53cc19
ting3242006 <ting3242006@hotmail.com>
2021-07-16 12:12:28 +0800

    Initial Commit

---
+++ Haikyu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Haikyu
+//
+//  Created by Ting on 2021/7/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
